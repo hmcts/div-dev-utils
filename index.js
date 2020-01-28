ccb3004267e0198ca81f592742a2e2bf92fb844d
J. Victor Ferreira <victor.ferreira@hmcts.net>
2020-01-28 17:04:53 +0000

    Works but needs tidying up

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 const dateTime = require('date-and-time');
+const child_process = require('child_process');
+
+const hostnameAndPort = 'div-cos-aat.service.core-compute-aat.internal';
+const startDate = dateTime.parse('2020-01-28', 'YYYY-MM-DD');
 
 const today = new Date();
-const startDate = dateTime.parse('2019-10-01', 'YYYY-MM-DD');
+const status = 'DA';
 
-console.log(dateTime.format(startDate, 'YYYY-MM-DD'));
-console.log(dateTime.format(today, 'YYYY-MM-DD'));
+console.log(`Extracting data from ${dateTime.format(startDate, 'YYYY-MM-DD')} to ${dateTime.format(today, 'YYYY-MM-DD')}`);
 
+// child_process.exec('export http_proxy="http://proxyout.reform.hmcts.net:8080"', (error, stout, sterr) => {
+    // if (error) throw error;
 
-let dateToProcess = startDate;
-while (dateToProcess <= today){
-    const formattedDateToProcess = dateTime.format(dateToProcess, 'YYYY-MM-DD');
-    console.log(`Will process ${formattedDateToProcess}`);
-    
-    dateToProcess = dateTime.addDays(dateToProcess, 1);
-}
+    let dateToProcess = startDate;
+    while (dateToProcess <= today) {
+        const formattedDateToProcess = dateTime.format(dateToProcess, 'YYYY-MM-DD');
+
+        const commandLineToRun = `export http_proxy="http://proxyout.reform.hmcts.net:8080" && curl -k -d '' http://${hostnameAndPort}/cases/data-extraction/family-man/status/${status}/lastModifiedDate/${formattedDateToProcess}`;
+        console.log(`command line is: ${commandLineToRun}`);
+        child_process.exec(commandLineToRun, (error, stout, sterr) => {
+            console.log(error);
+
+            if (error) {
+                console.log(`Error for ${formattedDateToProcess}: ${error}`);
+            } else {
+                console.log(`Success for ${formattedDateToProcess}. Response is: ${stout}`);
+            }
+
+        });
+
+        dateToProcess = dateTime.addDays(dateToProcess, 1);
+    }
+
+// });
